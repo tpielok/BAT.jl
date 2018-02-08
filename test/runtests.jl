8741f6ba654ea3be48ff6bbd022e3b9e35730202
Tobias Pielok <tpielok@mpp.mpg.de>
2018-02-08 17:30:44 +0100

    test only const_density

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,15 @@
 
 import Compat.Test
 Test.@testset "Package BAT" begin
-    include("mcmc_rand.jl")
-    include("data_vector.jl")
-    include("rng.jl")
-    include("density.jl")
-    include("distributions.jl")
-    include("proposaldist.jl")
-    include("spatialvolume.jl")
-    include("parambounds.jl")
-    include("onlinemvstats.jl")
-    include("onlineuvstats.jl")
+    # include("mcmc_rand.jl")
+    # include("data_vector.jl")
+    # include("rng.jl")
+    # include("density.jl")
+    # include("distributions.jl")
+    # include("proposaldist.jl")
+    # include("spatialvolume.jl")
+    # include("parambounds.jl")
+    # include("onlinemvstats.jl")
+    # include("onlineuvstats.jl")
     include("const_density.jl")
 end
